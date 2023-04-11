14574f4f060ba5944525cd97b6605587e6f6778c
HiddenL123 <75058857+HiddenL123@users.noreply.github.com>
2023-04-11 16:18:52 -0600

    Add files via upload

---
+++ include/global.hpp
@@ -0,0 +1,9 @@
+#include "EZ-Template/api.hpp"
+extern pros::Motor incata_r;
+extern pros::Motor incata_l;
+extern pros::ADIDigitalOut launcher;
+extern pros::ADIDigitalIn catalim;
+extern pros::ADIDigitalOut piston_boost;
+
+extern bool expansionExtend;
+extern bool cataOverride;
---
+++ include/pathfinder.hpp
@@ -0,0 +1,2 @@
+extern void go_to(float x, float y, float drive_speed, float turn_speed);
+extern void swerve();
